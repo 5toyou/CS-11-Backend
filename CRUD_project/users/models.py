e605c33a8d2ac3431bbad8ac417214ec4d771a89
5toyou <sobkos934@gmail.com>
2026-03-16 17:49:22 +0200

    commit 2

--- CRUD_project/users/models.py
+++ CRUD_project/users/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError('Phone number is required')
+        extra_fields.setdefault('is_active', True)
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
+        return self.create_user(phone, password, **extra_fields)
+    
+objects = CustomUserManager()
+
+
+class CustomUser(AbstractUser):
+    # Remove username, use phone instead
+    username = None
+    phone = models.CharField(max_length=20, unique=True)
+    role = models.CharField(
+        max_length=10,
+        choices=[('admin', 'Administrator'), ('teacher', 'Teacher')],
+        default='teacher',
+    )
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name} ({self.phone}")
