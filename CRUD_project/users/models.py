d6e6f9394fca99650ef65cad31169ea260ae1490
5toyou <sobkos934@gmail.com>
2026-03-18 14:35:09 +0200

    fixed

--- CRUD_project/users/models.py
+++ CRUD_project/users/models.py
@@ -4,38 +4,37 @@ from django.db import models
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, phone, password=None, **extra_fields):
-        if not phone:
-            raise ValueError('Phone number is required')
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_active', True)
-        user = self.model(phone=phone, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, phone, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('role', 'admin')
 
-        return self.create_user(phone, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
     
 
 
 class CustomUser(AbstractUser):
-    # Remove username, use phone instead
     username = None
-    phone = models.CharField(max_length=20, unique=True)
-    role = models.CharField(
-        max_length=10,
-        choices=[('admin', 'Administrator'), ('teacher', 'Teacher')],
-        default='teacher',
-    )
+    email = models.CharField(max_length=100, unique=True)
+    role = models.CharField(max_length=10,default='user')
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'phone'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.phone})"
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+
+
+class Books(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
--- CRUD_project/users/views.py
+++ CRUD_project/users/views.py
@@ -2,24 +2,33 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from users.models import Books
 
 def login_view(request):
     if request.method == 'POST':
-        phone = request.POST.get('phone')
+        email = request.POST.get('email')
         password = request.POST.get('password')
 
-        user = authenticate(request, phone=phone, password=password)
+        user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('dashboard')
         else:
-            return render(request, 'users/templates/login.html', {
+            return render(request, 'users/login.html', {
                 'error': 'Invalid credentials'
             })
         
-    return render(request, 'users/templates/login.html')
+    return render(request, 'users/login.html')
 
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def Books_page(request):
+    books = Books.objects.all()
+    context = {
+        'books_list': books,
+    }
+
+    return render(request,context)
