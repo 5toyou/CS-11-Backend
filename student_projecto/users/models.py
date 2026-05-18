bb2c90a41d191d0fe48c140ad2b1c2a7d4b3fa3f
5toyou <sobkos934@gmail.com>
2026-05-18 18:20:44 +0300

    everyone is staff fix

--- student_projecto/users/models.py
+++ student_projecto/users/models.py
@@ -18,8 +18,8 @@ class Branch(models.Model):
 class CustomUserManager(BaseUserManager):
     def create_user(self, phone, password=None, **extra_fields):
         extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
         user = self.model(phone=phone, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -28,7 +28,10 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, phone, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('role', 'admin')
+        extra_fields.setdefault('role', 'ADMIN')
+
+        if extra_fields.get('role') != 'ADMIN':
+            raise ValueError('Superuser must have role="ADMIN".')
 
         return self.create_user(phone, password, **extra_fields)
 
